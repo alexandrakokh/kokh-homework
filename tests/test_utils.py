ac3e7d240239f65b1b4dcb5995834c308a70f1ac
Александра Кох <plahotnikova.al@gmail.com>
2026-06-03 15:34:54 +0400

    fix tests utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,26 +2,23 @@ import unittest
 from unittest.mock import patch, mock_open
 from src.utils import read_json_file
 
-
 class TestReadJsonFile(unittest.TestCase):
 
-
-    @patch('builtins.open', mock_open(read_data='[]'))
     @patch('os.path.exists', return_value=True)
-    def test_empty_list(self, mock_exists, mock_file):
+    @patch('builtins.open', new_callable=mock_open, read_data='[]')
+    def test_empty_list(self, mock_file, mock_exists):
         result = read_json_file('test.json')
         self.assertEqual(result, [])
 
-
-    @patch('builtins.open', mock_open(read_data='"not a list"'))
     @patch('os.path.exists', return_value=True)
-    def test_not_a_list(self, mock_exists, mock_file):
+    @patch('builtins.open', new_callable=mock_open, read_data='"not a list"')
+    def test_not_a_list(self, mock_file, mock_exists):
         result = read_json_file('test.json')
         self.assertEqual(result, [])
 
-    @patch('builtins.open', mock_open(read_data='[{"id": 1, "amount": 100}]'))
     @patch('os.path.exists', return_value=True)
-    def test_valid_list(self, mock_exists, mock_file):
+    @patch('builtins.open', new_callable=mock_open, read_data='[{"id": 1, "amount": 100}]')
+    def test_valid_list(self, mock_file, mock_exists):
         result = read_json_file('test.json')
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]['id'], 1)
